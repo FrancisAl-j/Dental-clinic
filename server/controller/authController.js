bf04101f559fffcf6e2ee39e71f198cbff1df01b
Francis <kikobilas123@gmail.com>
2024-08-06 16:57:55 +0800

    create an admin registration

---
+++ server/controller/authController.js
@@ -0,0 +1,28 @@
+import bcryptjs from "bcryptjs";
+import Admin from "../models/adminModel.js";
+
+// Admin registration
+const adminRegister = async (req, res, next) => {
+  const { username, email, password, Cpassword } = req.body;
+  if (Cpassword !== password) {
+    return res.status(400).json("Password do not match");
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newAdmin = new Admin({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newAdmin.save();
+    res.status(200).json("Admin created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  adminRegister,
+};
--- server/router/authRoute.js
+++ server/router/authRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
+import controller from "../controller/authController.js";
 
 const router = express.Router();
 
-router.post("/admin/signup");
+router.post("/admin/signup", controller.adminRegister);
 
 export default router;
