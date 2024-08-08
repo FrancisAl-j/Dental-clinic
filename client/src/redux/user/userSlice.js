7c2f05f41ed20692055806e7269a29853c415149
Francis <kikobilas123@gmail.com>
2024-08-08 19:44:34 +0800

    added some things

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -9,7 +9,26 @@ const initialState = {
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSucces: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = {
+        message: action.payload.message,
+        code: action.payload.code,
+        status: action.payload.response ? action.payload.response.status : null,
+      };
+    },
+  },
 });
 
 export default userSlice.reducer;
