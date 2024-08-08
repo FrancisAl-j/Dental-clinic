0a7614c65bb855bf28a7ee317485c91eb216f6bb
Francis <kikobilas123@gmail.com>
2024-08-08 18:07:34 +0800

    Finished backend fir creating clinic

--- server/controller/clinicController.js
+++ server/controller/clinicController.js
@@ -1,7 +1,25 @@
+import Admin from "../models/adminModel.js";
 import Clinic from "../models/clinicModel.js";
 
 const createClinic = async (req, res, next) => {
   const { clinicName, location, email, phone } = req.body;
+  try {
+    const admin = await Admin.findById(req.admin.id);
+    if (!admin) {
+      return res.status(400).json("Unathenticated user!");
+    }
+
+    const clinic = new Clinic({ clinicName, location, email, phoe });
+    await clinic.save();
+
+    admin.clinicId = clinic._id;
+
+    await admin.save();
+
+    rse.status(200).json(clinic);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
