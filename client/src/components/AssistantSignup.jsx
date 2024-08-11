9375fd49a140a2f0da5762f39fdaa98158c59a3d
Francis <kikobilas123@gmail.com>
2024-08-11 20:42:42 +0800

    Finished UL for creating assistant (No design)

--- client/src/components/AssistantSignup.jsx
+++ client/src/components/AssistantSignup.jsx
@@ -1,7 +1,33 @@
 const AssistantSignup = () => {
   return (
-    <div>
+    <div className="form-container">
       <h1>Create Account for assistant</h1>
+
+      <div className="form-wrapper">
+        <form>
+          <div className="form-element">
+            <span>Username</span>
+            <input type="text" name="username" />
+          </div>
+
+          <div className="form-element">
+            <span>email</span>
+            <input type="email" name="email" />
+          </div>
+
+          <div className="form-element">
+            <span>password</span>
+            <input type="password" name="password" />
+          </div>
+
+          <div className="form-element">
+            <span>Confirm Password</span>
+            <input type="password" name="Cpassword" />
+          </div>
+
+          <button>Create</button>
+        </form>
+      </div>
     </div>
   );
 };
