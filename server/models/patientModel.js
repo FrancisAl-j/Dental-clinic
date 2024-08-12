00fcd75d81419cd88dd64cbc29bec676f454aae4
Francis <kikobilas123@gmail.com>
2024-08-12 22:10:04 +0800

    Craeted a patient model

---
+++ server/models/patientModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const patientSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Patient = mongoose.model("Patient", patientSchema);
+export default Patient;
