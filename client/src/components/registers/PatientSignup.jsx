4944d6851f70c4da9bfb641eceefc56bb875603a
Francis <kikobilas123@gmail.com>
2024-08-12 23:40:08 +0800

    Finished the UI for creating patient account

---
+++ client/src/components/registers/PatientSignup.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const PatientSignup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    Cpassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <h1>Create an account</h1>
+
+      <div className="form-wrapper">
+        <form>
+          <div className="form-element">
+            <span>Username</span>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-element">
+            <span>Email</span>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-element">
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-element">
+            <span>Confirm Password</span>
+            <input
+              type="password"
+              name="Cpassword"
+              value={formData.Cpassword}
+              onChange={handleChange}
+            />
+          </div>
+          <button>Create</button>
+        </form>
+      </div>
+    </div>
+  );
+};
