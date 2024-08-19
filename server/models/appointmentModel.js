7620a407ac8c64e534efea682abfbb167ad8d05d
Francis <kikobilas123@gmail.com>
2024-08-20 00:35:50 +0800

    created an appointment model

---
+++ server/models/appointmentModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = mongoose.Schema(
+  {
+    patientName: {
+      type: String,
+      required: true,
+    },
+    patientAge: {
+      type: Number,
+      required: true,
+    },
+    patientGender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    clinicId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Clinic",
+      required: true,
+    },
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    appointmentDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Completed", "Canceled"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+export default Appointment;
