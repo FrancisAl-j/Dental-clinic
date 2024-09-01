85e3d7fd66b9612b4154a0dea05aba223d690c3a
Francis <kikobilas123@gmail.com>
2024-09-01 17:07:15 +0800

    Search filtering working

--- client/src/components/Chart/DentalChart.jsx
+++ client/src/components/Chart/DentalChart.jsx
@@ -1,9 +1,13 @@
+import axios from "axios";
 import { toPng } from "html-to-image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const DentalChart = () => {
   const elementRef = useRef(null);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
+  // Converting form to image
   const htmlToImage = () => {
     toPng(elementRef.current, { cacheBust: false })
       .then((dataUrl) => {
@@ -17,9 +21,40 @@ const DentalChart = () => {
       });
   };
 
+  // Searching names of patients
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.get("http://localhost:5000/search/names", {
+        params: { query },
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        setResults(res.data);
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <form ref={elementRef}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search Patients"
+        />
+        <button onClick={handleSearch}>Search</button>
+        {results.map((patient) => {
+          return (
+            <div key={patient._id}>
+              <h1>{patient.patientName}</h1>
+            </div>
+          );
+        })}
         <div className="elements">
           <input type="text" placeholder="First Name" />
           <input type="text" placeholder="Surname" />
