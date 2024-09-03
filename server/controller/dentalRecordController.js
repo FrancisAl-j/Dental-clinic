aecd64f2287c2e2eeb4171011a4894d79befb11e
Francis <kikobilas123@gmail.com>
2024-09-03 23:36:20 +0800

    Created an api for creating dental records for patients

--- server/controller/dentalRecordController.js
+++ server/controller/dentalRecordController.js
@@ -1,7 +1,25 @@
+import Admin from "../models/adminModel.js";
 import DentalRecord from "../models/dentalRecordModel.js";
 
 const createDentalRecord = async (req, res, next) => {
+  const { patientId, treatments, conditions, chartDetails } = req.body;
   try {
+    const admin = await Admin.findById(req.user.id);
+    if (!admin) {
+      return res.status(401).json({ message: "Admin unauthenticated" });
+    }
+
+    const newDentalRecord = new DentalRecord({
+      patientId,
+      treatments,
+      conditions,
+      chartDetails,
+      clinicId: admin.clinicId,
+    });
+
+    await newDentalRecord.save();
+
+    res.status(200).json({ message: "Dental record created" });
   } catch (error) {
     next(error);
   }
--- server/router/dentalRecordRoute.js
+++ server/router/dentalRecordRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-//import controller from "../controller/dentalRecordController.js";
 import controller from "../controller/dentalRecordController.js";
+import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/create-record", controller.createDentalRecord);
+router.post("/create-record", verifyUser, controller.createDentalRecord);
 
 export default router;
